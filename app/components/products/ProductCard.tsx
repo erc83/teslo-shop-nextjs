e770d2019dbccd999005fd4b82be9cf1abea0ca9
erc83 <espinoza.eric@gmail.com>
2023-06-26 22:48:24 -0400

    ProductCard isHovered

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,8 +1,8 @@
 
 
 import { IProduct } from '@/interfaces'
-import { Card, CardActionArea, CardMedia, Grid } from '@mui/material'
-import { FC } from 'react'
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { FC, useMemo, useState } from 'react'
 
 interface Props {       // va a ser un producto de type IProduct
     product: IProduct;   // solo es uno no es un arreglo
@@ -10,19 +10,43 @@ interface Props {       // va a ser un producto de type IProduct
 
 
 export const ProductCard: FC<Props> = ( {product} ) => {   //{product}   obligo a tomar el producto
-  return (
-    <Grid item xs={6} sm={4} key={ product.slug}>
+    
+    // para saber cuando el mouse esta encima y cuando no, lo puedo llamar en el GRID
+    const [isHovered, setIsHovered] = useState(false);
+
+    // memorizar para no calcular siempre
+    const productImage = useMemo(() => {
+        return isHovered
+            ? `products/${ product.images[1]}`
+            : `products/${ product.images[0]}`
+    }, [isHovered, product.images])
+    
+    return (
+    <Grid item xs={6} sm={4}
+        onMouseEnter={ () => setIsHovered( true )}
+        onMouseLeave={ () => setIsHovered( false )}
+    >
         <Card>
             <CardActionArea>
                 <CardMedia
                     component='img'
-                    image={`products/${ product.images[0]}`}
+                    className='fadeIn'
+                    
+                    image={ productImage }
                     alt={ product.title }
-                >
+                    
+                    onLoad={ () => console.log('cargo')}
+                />
 
-                </CardMedia>
             </CardActionArea>
         </Card>
+
+        {/* fadeIn en los stylos css globales */}
+        <Box sx={{ mt: 1 }} className="fadeIn">
+            <Typography fontWeight={700}>{ product.title}</Typography>
+            <Typography fontWeight={500}>{ `$${product.price}` }</Typography>
+        </Box>
+
     </Grid>
   )
 }
