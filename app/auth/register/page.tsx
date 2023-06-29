8c945c0146a79881ff40669f28c86685278ced38
erc83 <espinoza.eric@gmail.com>
2023-06-28 23:37:30 -0400

    registerPage, /auth/register

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import NextLink from 'next/link';
+import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material"
+
+import { AuthLayout } from "../../components/layouts"
+
+const registerPage = () => {
+  return (
+    <AuthLayout title={'Ingresar'}>
+        <Box sx={{ width:350, padding:'10px 20px' }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography variant='h1' component='h1'>Registrarse</Typography>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <TextField label='Nombre' variant='filled' fullWidth /> 
+                </Grid>
+                
+                <Grid item xs={12}>
+                    <TextField label='Correo' variant='filled' fullWidth /> 
+                </Grid>
+
+                <Grid item xs={12}>
+                    <TextField label='Contraseña' type='password' variant='filled' fullWidth /> 
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField label='Confirmar Contraseña' type='password' variant='filled' fullWidth /> 
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Button color='secondary' className='circular-btn' size='large' fullWidth>
+                        Ingresar
+                    </Button>
+                </Grid>
+                
+                <Grid item xs={12} display='flex' justifyContent='end'>
+                    <NextLink href='/auth/login' passHref legacyBehavior>
+                        <Link underline='always'>
+                            ¿Ya tienes una cuenta?
+                        </Link>
+                    </NextLink>
+                </Grid>
+
+
+
+            </Grid>
+        </Box>
+    </AuthLayout>
+  )
+}
+
+export default registerPage
